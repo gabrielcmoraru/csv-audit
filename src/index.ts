399dbc1a7c368c8228490c3c4d31bc0e1e4ccedc
Gabriel Moraru <gabrielcmoraru@github.com>
2021-07-29 18:06:26 +0100

    refactor file reader to abstract with generic types class; move logic to match reader

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
 import { MatchResult } from './types';
 
-const reader = new CsvFileReader('football.csv');
-reader.read;
+const reader = new MatchReader('football.csv');
+reader.read();
 
 let manUnitedWins = 0;
 
